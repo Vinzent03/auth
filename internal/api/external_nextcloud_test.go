70c56b77744354e63cba7add69fba239fbcf6bd4
Vinzent <vinzent03@proton.me>
2025-03-11 12:45:07 +0100

    replace all occurence of gitlab

--- internal/api/external_nextcloud_test.go
+++ internal/api/external_nextcloud_test.go
@@ -10,21 +10,21 @@ import (
 )
 
 const (
-	gitlabUser           string = `{"id":123,"email":"gitlab@example.com","name":"GitLab Test","avatar_url":"http://example.com/avatar","confirmed_at":"2012-05-23T09:05:22Z"}`
-	gitlabUserWrongEmail string = `{"id":123,"email":"other@example.com","name":"GitLab Test","avatar_url":"http://example.com/avatar","confirmed_at":"2012-05-23T09:05:22Z"}`
-	gitlabUserNoEmail    string = `{"id":123,"name":"Gitlab Test","avatar_url":"http://example.com/avatar"}`
+	nextcloudUser           string = `{"id":123,"email":"nextcloud@example.com","name":"Nextcloud Test","avatar_url":"http://example.com/avatar","confirmed_at":"2012-05-23T09:05:22Z"}`
+	nextcloudUserWrongEmail string = `{"id":123,"email":"other@example.com","name":"Nextcloud Test","avatar_url":"http://example.com/avatar","confirmed_at":"2012-05-23T09:05:22Z"}`
+	nextcloudUserNoEmail    string = `{"id":123,"name":"Nextcloud Test","avatar_url":"http://example.com/avatar"}`
 )
 
-func (ts *ExternalTestSuite) TestSignupExternalGitlab() {
-	req := httptest.NewRequest(http.MethodGet, "http://localhost/authorize?provider=gitlab", nil)
+func (ts *ExternalTestSuite) TestSignupExternalNextcloud() {
+	req := httptest.NewRequest(http.MethodGet, "http://localhost/authorize?provider=nextcloud", nil)
 	w := httptest.NewRecorder()
 	ts.API.handler.ServeHTTP(w, req)
 	ts.Require().Equal(http.StatusFound, w.Code)
 	u, err := url.Parse(w.Header().Get("Location"))
 	ts.Require().NoError(err, "redirect url parse failed")
 	q := u.Query()
-	ts.Equal(ts.Config.External.Gitlab.RedirectURI, q.Get("redirect_uri"))
-	ts.Equal(ts.Config.External.Gitlab.ClientID, []string{q.Get("client_id")})
+	ts.Equal(ts.Config.External.Nextcloud.RedirectURI, q.Get("redirect_uri"))
+	ts.Equal(ts.Config.External.Nextcloud.ClientID, []string{q.Get("client_id")})
 	ts.Equal("code", q.Get("response_type"))
 	ts.Equal("read_user", q.Get("scope"))
 
@@ -35,21 +35,21 @@ func (ts *ExternalTestSuite) TestSignupExternalGitlab() {
 	})
 	ts.Require().NoError(err)
 
-	ts.Equal("gitlab", claims.Provider)
+	ts.Equal("nextcloud", claims.Provider)
 	ts.Equal(ts.Config.SiteURL, claims.SiteURL)
 }
 
-func GitlabTestSignupSetup(ts *ExternalTestSuite, tokenCount *int, userCount *int, code string, user string, emails string) *httptest.Server {
+func NextcloudTestSignupSetup(ts *ExternalTestSuite, tokenCount *int, userCount *int, code string, user string, emails string) *httptest.Server {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch r.URL.Path {
 		case "/oauth/token":
 			*tokenCount++
 			ts.Equal(code, r.FormValue("code"))
 			ts.Equal("authorization_code", r.FormValue("grant_type"))
-			ts.Equal(ts.Config.External.Gitlab.RedirectURI, r.FormValue("redirect_uri"))
+			ts.Equal(ts.Config.External.Nextcloud.RedirectURI, r.FormValue("redirect_uri"))
 
 			w.Header().Add("Content-Type", "application/json")
-			fmt.Fprint(w, `{"access_token":"gitlab_token","expires_in":100000}`)
+			fmt.Fprint(w, `{"access_token":"nextcloud_token","expires_in":100000}`)
 		case "/api/v4/user":
 			*userCount++
 			w.Header().Add("Content-Type", "application/json")
@@ -59,141 +59,141 @@ func GitlabTestSignupSetup(ts *ExternalTestSuite, tokenCount *int, userCount *in
 			fmt.Fprint(w, emails)
 		default:
 			w.WriteHeader(500)
-			ts.Fail("unknown gitlab oauth call %s", r.URL.Path)
+			ts.Fail("unknown nextcloud oauth call %s", r.URL.Path)
 		}
 	}))
 
-	ts.Config.External.Gitlab.URL = server.URL
+	ts.Config.External.Nextcloud.URL = server.URL
 
 	return server
 }
 
-func (ts *ExternalTestSuite) TestSignupExternalGitlab_AuthorizationCode() {
-	// additional emails from GitLab don't return confirm status
+func (ts *ExternalTestSuite) TestSignupExternalNextcloud_AuthorizationCode() {
+	// additional emails from Nextcloud don't return confirm status
 	ts.Config.Mailer.Autoconfirm = true
 
 	tokenCount, userCount := 0, 0
 	code := "authcode"
-	emails := `[{"id":1,"email":"gitlab@example.com"}]`
-	server := GitlabTestSignupSetup(ts, &tokenCount, &userCount, code, gitlabUser, emails)
+	emails := `[{"id":1,"email":"nextcloud@example.com"}]`
+	server := NextcloudTestSignupSetup(ts, &tokenCount, &userCount, code, nextcloudUser, emails)
 	defer server.Close()
 
-	u := performAuthorization(ts, "gitlab", code, "")
+	u := performAuthorization(ts, "nextcloud", code, "")
 
-	assertAuthorizationSuccess(ts, u, tokenCount, userCount, "gitlab@example.com", "GitLab Test", "123", "http://example.com/avatar")
+	assertAuthorizationSuccess(ts, u, tokenCount, userCount, "nextcloud@example.com", "Nextcloud Test", "123", "http://example.com/avatar")
 }
 
-func (ts *ExternalTestSuite) TestSignupExternalGitLabDisableSignupErrorWhenNoUser() {
+func (ts *ExternalTestSuite) TestSignupExternalNextcloudDisableSignupErrorWhenNoUser() {
 	ts.Config.DisableSignup = true
 
 	tokenCount, userCount := 0, 0
 	code := "authcode"
-	emails := `[{"id":1,"email":"gitlab@example.com"}]`
-	server := GitlabTestSignupSetup(ts, &tokenCount, &userCount, code, gitlabUser, emails)
+	emails := `[{"id":1,"email":"nextcloud@example.com"}]`
+	server := NextcloudTestSignupSetup(ts, &tokenCount, &userCount, code, nextcloudUser, emails)
 	defer server.Close()
 
-	u := performAuthorization(ts, "gitlab", code, "")
+	u := performAuthorization(ts, "nextcloud", code, "")
 
-	assertAuthorizationFailure(ts, u, "Signups not allowed for this instance", "access_denied", "gitlab@example.com")
+	assertAuthorizationFailure(ts, u, "Signups not allowed for this instance", "access_denied", "nextcloud@example.com")
 }
 
-func (ts *ExternalTestSuite) TestSignupExternalGitLabDisableSignupErrorWhenEmptyEmail() {
+func (ts *ExternalTestSuite) TestSignupExternalNextcloudDisableSignupErrorWhenEmptyEmail() {
 	ts.Config.DisableSignup = true
 
 	tokenCount, userCount := 0, 0
 	code := "authcode"
 	emails := `[]`
-	server := GitlabTestSignupSetup(ts, &tokenCount, &userCount, code, gitlabUserNoEmail, emails)
+	server := NextcloudTestSignupSetup(ts, &tokenCount, &userCount, code, nextcloudUserNoEmail, emails)
 	defer server.Close()
 
-	u := performAuthorization(ts, "gitlab", code, "")
+	u := performAuthorization(ts, "nextcloud", code, "")
 
-	assertAuthorizationFailure(ts, u, "Error getting user email from external provider", "server_error", "gitlab@example.com")
+	assertAuthorizationFailure(ts, u, "Error getting user email from external provider", "server_error", "nextcloud@example.com")
 }
 
-func (ts *ExternalTestSuite) TestSignupExternalGitLabDisableSignupSuccessWithPrimaryEmail() {
+func (ts *ExternalTestSuite) TestSignupExternalNextcloudDisableSignupSuccessWithPrimaryEmail() {
 	ts.Config.DisableSignup = true
 
-	ts.createUser("123", "gitlab@example.com", "GitLab Test", "http://example.com/avatar", "")
+	ts.createUser("123", "nextcloud@example.com", "Nextcloud Test", "http://example.com/avatar", "")
 
 	tokenCount, userCount := 0, 0
 	code := "authcode"
 	emails := "[]"
-	server := GitlabTestSignupSetup(ts, &tokenCount, &userCount, code, gitlabUser, emails)
+	server := NextcloudTestSignupSetup(ts, &tokenCount, &userCount, code, nextcloudUser, emails)
 	defer server.Close()
 
-	u := performAuthorization(ts, "gitlab", code, "")
+	u := performAuthorization(ts, "nextcloud", code, "")
 
-	assertAuthorizationSuccess(ts, u, tokenCount, userCount, "gitlab@example.com", "GitLab Test", "123", "http://example.com/avatar")
+	assertAuthorizationSuccess(ts, u, tokenCount, userCount, "nextcloud@example.com", "Nextcloud Test", "123", "http://example.com/avatar")
 }
 
-func (ts *ExternalTestSuite) TestSignupExternalGitLabDisableSignupSuccessWithSecondaryEmail() {
-	// additional emails from GitLab don't return confirm status
+func (ts *ExternalTestSuite) TestSignupExternalNextcloudDisableSignupSuccessWithSecondaryEmail() {
+	// additional emails from Nextcloud don't return confirm status
 	ts.Config.Mailer.Autoconfirm = true
 	ts.Config.DisableSignup = true
 
-	ts.createUser("123", "secondary@example.com", "GitLab Test", "http://example.com/avatar", "")
+	ts.createUser("123", "secondary@example.com", "Nextcloud Test", "http://example.com/avatar", "")
 
 	tokenCount, userCount := 0, 0
 	code := "authcode"
 	emails := `[{"id":1,"email":"secondary@example.com"}]`
-	server := GitlabTestSignupSetup(ts, &tokenCount, &userCount, code, gitlabUser, emails)
+	server := NextcloudTestSignupSetup(ts, &tokenCount, &userCount, code, nextcloudUser, emails)
 	defer server.Close()
 
-	u := performAuthorization(ts, "gitlab", code, "")
+	u := performAuthorization(ts, "nextcloud", code, "")
 
-	assertAuthorizationSuccess(ts, u, tokenCount, userCount, "secondary@example.com", "GitLab Test", "123", "http://example.com/avatar")
+	assertAuthorizationSuccess(ts, u, tokenCount, userCount, "secondary@example.com", "Nextcloud Test", "123", "http://example.com/avatar")
 }
 
-func (ts *ExternalTestSuite) TestInviteTokenExternalGitLabSuccessWhenMatchingToken() {
-	// name and avatar should be populated from GitLab API
-	ts.createUser("123", "gitlab@example.com", "", "", "invite_token")
+func (ts *ExternalTestSuite) TestInviteTokenExternalNextcloudSuccessWhenMatchingToken() {
+	// name and avatar should be populated from Nextcloud API
+	ts.createUser("123", "nextcloud@example.com", "", "", "invite_token")
 
 	tokenCount, userCount := 0, 0
 	code := "authcode"
 	emails := "[]"
-	server := GitlabTestSignupSetup(ts, &tokenCount, &userCount, code, gitlabUser, emails)
+	server := NextcloudTestSignupSetup(ts, &tokenCount, &userCount, code, nextcloudUser, emails)
 	defer server.Close()
 
-	u := performAuthorization(ts, "gitlab", code, "invite_token")
+	u := performAuthorization(ts, "nextcloud", code, "invite_token")
 
-	assertAuthorizationSuccess(ts, u, tokenCount, userCount, "gitlab@example.com", "GitLab Test", "123", "http://example.com/avatar")
+	assertAuthorizationSuccess(ts, u, tokenCount, userCount, "nextcloud@example.com", "Nextcloud Test", "123", "http://example.com/avatar")
 }
 
-func (ts *ExternalTestSuite) TestInviteTokenExternalGitLabErrorWhenNoMatchingToken() {
+func (ts *ExternalTestSuite) TestInviteTokenExternalNextcloudErrorWhenNoMatchingToken() {
 	tokenCount, userCount := 0, 0
 	code := "authcode"
 	emails := "[]"
-	server := GitlabTestSignupSetup(ts, &tokenCount, &userCount, code, gitlabUser, emails)
+	server := NextcloudTestSignupSetup(ts, &tokenCount, &userCount, code, nextcloudUser, emails)
 	defer server.Close()
 
-	w := performAuthorizationRequest(ts, "gitlab", "invite_token")
+	w := performAuthorizationRequest(ts, "nextcloud", "invite_token")
 	ts.Require().Equal(http.StatusNotFound, w.Code)
 }
 
-func (ts *ExternalTestSuite) TestInviteTokenExternalGitLabErrorWhenWrongToken() {
-	ts.createUser("123", "gitlab@example.com", "", "", "invite_token")
+func (ts *ExternalTestSuite) TestInviteTokenExternalNextcloudErrorWhenWrongToken() {
+	ts.createUser("123", "nextcloud@example.com", "", "", "invite_token")
 
 	tokenCount, userCount := 0, 0
 	code := "authcode"
 	emails := "[]"
-	server := GitlabTestSignupSetup(ts, &tokenCount, &userCount, code, gitlabUser, emails)
+	server := NextcloudTestSignupSetup(ts, &tokenCount, &userCount, code, nextcloudUser, emails)
 	defer server.Close()
 
-	w := performAuthorizationRequest(ts, "gitlab", "wrong_token")
+	w := performAuthorizationRequest(ts, "nextcloud", "wrong_token")
 	ts.Require().Equal(http.StatusNotFound, w.Code)
 }
 
-func (ts *ExternalTestSuite) TestInviteTokenExternalGitLabErrorWhenEmailDoesntMatch() {
-	ts.createUser("123", "gitlab@example.com", "", "", "invite_token")
+func (ts *ExternalTestSuite) TestInviteTokenExternalNextcloudErrorWhenEmailDoesntMatch() {
+	ts.createUser("123", "nextcloud@example.com", "", "", "invite_token")
 
 	tokenCount, userCount := 0, 0
 	code := "authcode"
 	emails := "[]"
-	server := GitlabTestSignupSetup(ts, &tokenCount, &userCount, code, gitlabUserWrongEmail, emails)
+	server := NextcloudTestSignupSetup(ts, &tokenCount, &userCount, code, nextcloudUserWrongEmail, emails)
 	defer server.Close()
 
-	u := performAuthorization(ts, "gitlab", code, "invite_token")
+	u := performAuthorization(ts, "nextcloud", code, "invite_token")
 
 	assertAuthorizationFailure(ts, u, "Invited email does not match emails from external provider", "invalid_request", "")
 }
